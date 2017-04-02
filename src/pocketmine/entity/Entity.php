c3b86d9e1b3dcab6b85d2bd5d7e11d02dc3e385a
xXSirGamesXx <xXSirGamesXx@gmail.com>
2017-04-02 08:19:15 -0400

    Update Entity.php

--- src/pocketmine/entity/Entity.php
+++ src/pocketmine/entity/Entity.php
@@ -170,6 +170,7 @@ n	const DATA_FLAG_ELDER = 28; //elder guardian
 		Entity::registerEntity(Bat::class);
 		Entity::registerEntity(Blaze::class);
 		Entity::registerEntity(Boat::class);
+		Entity::registerEntity(BlueWitherSkull);
 		Entity::registerEntity(CaveSpider::class);
 		Entity::registerEntity(Chicken::class);
 		Entity::registerEntity(Cow::class);
@@ -179,14 +180,18 @@ n	const DATA_FLAG_ELDER = 28; //elder guardian
 		Entity::registerEntity(Enderman::class);
 		Entity::registerEntity(EnderMite::class);
 		Entity::registerEntity(EnderDragon::class);
+		Entity::registerEntity(ElderGuardian::class);
 		Entity::registerEntity(EnderPearl::class);
 		Entity::registerEntity(FallingSand::class);
 		Entity::registerEntity(FishingHook::class);
 		Entity::registerEntity(Ghast::class);
+		Entity::registerEntity(GhastFireBall::class);
+		Entity::registerEntity(Guardian::class);
 		Entity::registerEntity(Horse::class);
 		Entity::registerEntity(Husk::class);
 		Entity::registerEntity(IronGolem::class);
 		Entity::registerEntity(LavaSlime::class); //Magma Cube
+		Entity::registerEntity(LeachKnot::class);
 		Entity::registerEntity(Lightning::class);
 		Entity::registerEntity(Minecart::class);
 		Entity::registerEntity(MinecartChest::class);
@@ -197,9 +202,12 @@ n	const DATA_FLAG_ELDER = 28; //elder guardian
 		Entity::registerEntity(Painting::class);
 		Entity::registerEntity(Pig::class);
 		Entity::registerEntity(PigZombie::class);
+		Entity::registerEntity(PolarBear::class);
 		Entity::registerEntity(PrimedTNT::class);
 		Entity::registerEntity(Rabbit::class);
 		Entity::registerEntity(Sheep::class);
+		Entity::registerEntity(Shulker::class);
+		Entity::registerEntity(ShulkerBullet);
 		Entity::registerEntity(Silverfish::class);
 		Entity::registerEntity(Skeleton::class);
 		Entity::registerEntity(Slime::class);
@@ -213,6 +221,7 @@ n	const DATA_FLAG_ELDER = 28; //elder guardian
 		Entity::registerEntity(Villager::class);
 		Entity::registerEntity(Witch::class);
 		Entity::registerEntity(Wither::class);
+		Entity::registerEntity(WitherSkeleton::class);
 		Entity::registerEntity(Wolf::class);
 		Entity::registerEntity(XPOrb::class);
 		Entity::registerEntity(Zombie::class);
