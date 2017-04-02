f37b034c84617f5b706136c74faba73474a29a6d
xXSirGamesXx <xXSirGamesXx@gmail.com>
2017-04-02 19:51:15 -0400

    Create EnderMite.php

---
+++ src/pocketmine/entity/EnderMite.php
@@ -0,0 +1,25 @@
+<?php 
+
+/*
+  *     _____            _                _____ 
+  *    |  __ \          (_)              |____ |
+  *    | |  \/ ___ _ __  _ ___ _   _ ___     / /
+  *    | | __ / _ \ '_ \| / __| | | / __|    \ \
+  *    | |_\ \  __/ | | | \__ \ |_| \__ \.___/ /
+  *     \____/\___|_| |_|_|___/\__, |___/\____/ 
+  *                            __/ |           
+  *                           |___/            
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU Lesser General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * @author FrontierDevs
+  * @see Genisys3.org
+  */
+  
+  namespace pocketmine/entity;
+  
+  use pocketmine\network\protocol\AddEntityPacket;
+  use pocketmine\player;
