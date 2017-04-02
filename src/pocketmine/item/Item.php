20d5ef0da0eaf9897cc83f324adf7d276eb51210
xXSirGamesXx <xXSirGamesXx@gmail.com>
2017-04-02 13:56:04 -0400

    Update Item.php

--- src/pocketmine/item/Item.php
+++ src/pocketmine/item/Item.php
@@ -61,6 +61,7 @@ class Item implements ItemIds, \JsonSerializable{
 		return self::$cachedParser->getData();
 	}
 
+	
 	private static function writeCompoundTag(CompoundTag $tag) : string{
 		if(self::$cachedParser === null){
 			self::$cachedParser = new NBT(NBT::LITTLE_ENDIAN);
@@ -95,6 +96,7 @@ class Item implements ItemIds, \JsonSerializable{
 			self::$list[self::EYE_OF_ENDER] = EyeOfEnder::class;
 			self::$list[self::DRAGONS_BREATH] = DragonsBreath::class;
 			self::$list[self::SHULKER_SHELL] = ShulkerShell::class;
+			self::$list[self::CARROT_ON_A_STICK] = CarrotOnAStick::class;
 			self::$list[self::POPPED_CHORUS_FRUIT] = PoppedChorusFruit::class;
 			self::$list[self::WHEAT_SEEDS] = WheatSeeds::class;
 			self::$list[self::PUMPKIN_SEEDS] = PumpkinSeeds::class;
