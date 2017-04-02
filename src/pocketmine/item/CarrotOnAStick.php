037c41d015df52e4cb725df1bd33a88bf4a1bdc3
xXSirGamesXx <xXSirGamesXx@gmail.com>
2017-04-02 13:27:32 -0400

    Update CarrotOnAStick.php

--- src/pocketmine/item/CarrotOnAStick.php
+++ src/pocketmine/item/CarrotOnAStick.php
@@ -1,28 +1,29 @@
 <?php
 
-/*
- *
- *  _____   _____   __   _   _   _____  __    __  _____
- * /  ___| | ____| |  \ | | | | /  ___/ \ \  / / /  ___/
- * | |     | |__   |   \| | | | | |___   \ \/ /  | |___
- * | |  _  |  __|  | |\   | | | \___  \   \  /   \___  \
- * | |_| | | |___  | | \  | | |  ___| |   / /     ___| |
- * \_____/ |_____| |_|  \_| |_| /_____/  /_/     /_____/
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * @author iTX Technologies
- * @link https://itxtech.org
- *
- */
+ /*
+  *     _____            _                _____ 
+  *    |  __ \          (_)              |____ |
+  *    | |  \/ ___ _ __  _ ___ _   _ ___     / /
+  *    | | __ / _ \ '_ \| / __| | | / __|    \ \
+  *    | |_\ \  __/ | | | \__ \ |_| \__ \.___/ /
+  *     \____/\___|_| |_|_|___/\__, |___/\____/ 
+  *                            __/ |           
+  *                           |___/            
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU Lesser General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * @author FrontierDevs
+  * @see Genisys3.org
+
+  */
 
 namespace pocketmine\item;
 
-class FishingRod extends Item {
+class CarrotOnAStick extends Item {
 	public function __construct($meta = 0, $count = 1) {
-		parent::__construct(self::FISHING_ROD, 0, $count, "Fishing Rod");
+		parent::__construct(self::CARROT_ON_A_STICK, 0, $count, Carrot On a Stick");
 	}
 } 
