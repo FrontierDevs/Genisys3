31d5c1eb81b13e57cc2a220e3ca44f037cf3946f
eDroiid <edroiiid@gmail.com>
2017-04-01 18:29:54 -0700

    defaults

---
+++ src/pocketmine/command/defaults/BanListCommand.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ *
+ *
+*/
+
+namespace pocketmine\command\defaults;
+
+use pocketmine\command\CommandSender;
+use pocketmine\event\TranslationContainer;
+use pocketmine\Server;
+
+
+class BanListCommand extends VanillaCommand{
+
+	public function __construct($name){
+		parent::__construct(
+			$name,
+			"%pocketmine.command.banlist.description",
+			"%pocketmine.command.banlist.usage"
+		);
+		$this->setPermission("pocketmine.command.ban.list");
+	}
+
+	public function execute(CommandSender $sender, $currentAlias, array $args){
+		if(!$this->testPermission($sender)){
+			return true;
+		}
+		
+		$args[0] = (isset($args[0]) ? strtolower($args[0]): "");
+		$title = "";
+		
+		switch($args[0]){
+			case "ips":
+				$list = $sender->getServer()->getIPBans();	
+				$title = "commands.banlist.ips";
+				break;
+			case "cids":
+				$list = $list = $sender->getServer()->getCIDBans(); 
+				$title = "commands.banlist.cids";
+				break;
+			case "players":
+				$list = $sender->getServer()->getNameBans();
+				$title = "commands.banlist.players";
+				break;
+			default:
+				$sender->sendMessage(new TranslationContainer("commands.generic.usage", [$this->usageMessage]));
+				return false;			
+		}
+		
+		$message = "";
+		$list = $list->getEntries();
+		foreach($list as $entry){
+			$message .= $entry->getName() . ", ";
+		}
+		
+		$sender->sendMessage(Server::getInstance()->getLanguage()->translateString($title, [count($list)]));
+		$sender->sendMessage(\substr($message, 0, -2));
+
+		return true;
+	}
+}
