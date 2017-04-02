15e6b54e75d74fafd650ef3537c04b70c0a6582a
eDroiid <edroiiid@gmail.com>
2017-04-01 18:28:57 -0700

    blocks

---
+++ src/pocketmine/block/PackedIce.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ *
+ *  _____   _____   __   _   _   _____  __    __  _____
+ * /  ___| | ____| |  \ | | | | /  ___/ \ \  / / /  ___/
+ * | |     | |__   |   \| | | | | |___   \ \/ /  | |___
+ * | |  _  |  __|  | |\   | | | \___  \   \  /   \___  \
+ * | |_| | | |___  | | \  | | |  ___| |   / /     ___| |
+ * \_____/ |_____| |_|  \_| |_| /_____/  /_/     /_____/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author iTX Technologies
+ * @link https://itxtech.org
+ *
+ */
+
+namespace pocketmine\block;
+
+use pocketmine\item\Item;
+use pocketmine\item\Tool;
+use pocketmine\item\enchantment\Enchantment;
+
+class PackedIce extends Solid {
+
+	protected $id = self::PACKED_ICE;
+
+	public function __construct() {
+
+	}
+
+	public function getName() : string{
+		return "Packed Ice";
+	}
+
+	public function getHardness() {
+		return 0.5;
+	}
+
+	public function getToolType() {
+		return Tool::TYPE_PICKAXE;
+	}
+
+	public function getDrops(Item $item) : array {
+		if($item->getEnchantmentLevel(Enchantment::TYPE_MINING_SILK_TOUCH) > 0){
+			return [
+				[Item::PACKED_ICE, 0, 1],
+			];
+		}else{
+			return [];
+		}
+	}
+} 
---
+++ src/pocketmine/block/Podzol.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____  
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \ 
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/ 
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_| 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ * 
+ *
+*/
+
+namespace pocketmine\block;
+
+use pocketmine\item\Tool;
+use pocketmine\item\Item;
+use pocketmine\item\enchantment\Enchantment;
+
+class Podzol extends Solid{
+
+	protected $id = self::PODZOL;
+
+	public function __construct(){
+
+	}
+
+	public function getToolType(){
+		return Tool::TYPE_SHOVEL;
+	}
+
+	public function getName() : string{
+		return "Podzol";
+	}
+
+	public function getHardness() {
+		return 0.5;
+	}
+
+	public function getResistance(){
+		return 2.5;
+	}
+
+	public function getDrops(Item $item) : array{
+		if($item->getEnchantmentLevel(Enchantment::TYPE_MINING_SILK_TOUCH) > 0){
+			return [
+				[Item::PODZOL, 0, 1],
+			];
+		}else{
+			return [
+				[Item::DIRT, 0, 1],
+			];
+		}
+		
+	}
+}
