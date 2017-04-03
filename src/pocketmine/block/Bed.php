e37641d39c69f0514a58cacc45bda38fbf73a00d
xXSirGamesXx <xXSirGamesXx@gmail.com>
2017-04-03 12:26:35 -0400

    Update Bed.php

--- src/pocketmine/block/Bed.php
+++ src/pocketmine/block/Bed.php
@@ -30,6 +30,23 @@ use pocketmine\Player;
 use pocketmine\utils\TextFormat;
 
 class Bed extends Transparent{
+	
+	const WHITE = 0;
+	const ORANGE = 1;
+	const MAGENTA = 2;
+	const LIGHT_BLUE = 3;
+	const YELLOW = 4;
+	const LIME = 5;
+	const PINK = 6;
+	const GRAY = 7;
+	const LIGHT_GRAY = 8;
+	const CYAN = 9;
+	const PURPLE = 10;
+	const BLUE = 11;
+	const BROWN = 12;
+	const GREEN = 13;
+	const RED = 14;
+	const BLACK = 15;
 
 	protected $id = self::BED_BLOCK;
 
