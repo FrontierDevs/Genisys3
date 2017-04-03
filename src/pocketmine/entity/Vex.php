b66e700a2d57c8331b39d29f4608000c2bd9dec7
xXSirGamesXx <xXSirGamesXx@gmail.com>
2017-04-03 11:35:36 -0400

    Update Vex.php

--- src/pocketmine/entity/Vex.php
+++ src/pocketmine/entity/Vex.php
@@ -28,3 +28,43 @@ use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\Player;
 
 class Vex extends FlyingAnimal{
+ 
+ const NETWORK_ID = 105;
+ 
+ public $width = 0.6;
+ public $length = 0.6;
+ public $height = 0.7;
+ 
+ public $flySpeed = 0.8;
+ public $switchDirectionTicks = 100;
+ 
+public function getName() : string{
+         return "Vex";
+}
+public function initEntity(){
+        $this->setMaxHealth(14);
+        parent::initEntity();
+}
+	public function onUpdate($currentTick){
+		if ($this->age > 20 * 60 * 10) {
+			$this->kill();
+		}
+		return parent::onUpdate($currentTick);
+	}
+	public function spawnTo(Player $player){
+		$pk = new AddEntityPacket();
+		$pk->eid = $this->getId();
+		$pk->type = Bat::NETWORK_ID;
+		$pk->x = $this->x;
+		$pk->y = $this->y;
+		$pk->z = $this->z;
+		$pk->speedX = $this->motionX;
+		$pk->speedY = $this->motionY;
+		$pk->speedZ = $this->motionZ;
+		$pk->yaw = $this->yaw;
+		$pk->pitch = $this->pitch;
+		$pk->metadata = $this->dataProperties;
+		$player->dataPacket($pk);
+		parent::spawnTo($player);
+	     }
+}
